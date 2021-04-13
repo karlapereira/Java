03bbd5c6abd6d00d8cd19f413a0674243c67a342
Karla Pereira <karlapereira@gec.inatel.br>
2021-04-13 16:24:19 -0300

    Add files via upload

---
+++ Digimon/src/Control/Main.java
@@ -0,0 +1,16 @@
+package Control;
+
+import java.util.ArrayList;
+
+import Model.Digimon;
+import Model.Treinador;
+import View.Menu;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		
+		new Menu().setVisible(true);
+	}
+
+}
